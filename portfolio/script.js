5dd7edcbe91e747f0af44e93da6be564bd30568f
sreerag-a-sasi <sreeragakhd2002@gmail.com>
2024-11-21 12:34:26 +0530

    first commit

---
+++ portfolio/script.js
@@ -0,0 +1,30 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.carousel-slide');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    if (index >= totalSlides) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else {
+        currentSlide = index;
+    }
+
+    const offset = -currentSlide * 100;
+    document.querySelector('.carousel-container').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// Initially show the first slide
+showSlide(currentSlide);
+
+// Auto-slide functionality
+setInterval(nextSlide, 5000); // Change slide every 5 seconds
